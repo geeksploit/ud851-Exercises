f2450fdf37ac58650ccb2a4c80518e0422eb62b2
geeksploit <geeksploit@shootan.com>
2017-11-09 18:51:12 +0300

    Add a menu item to the menu

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
     // DONE (2) Create a menu xml called 'main.xml' in the res->menu folder
-    // TODO (3) Add one menu item to your menu
+    // DONE (3) Add one menu item to your menu
     // TODO (4) Give the menu item an id of @+id/action_search
     // TODO (5) Set the orderInCategory to 1
     // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
