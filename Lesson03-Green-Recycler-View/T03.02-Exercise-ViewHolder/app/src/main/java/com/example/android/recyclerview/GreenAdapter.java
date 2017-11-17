84db6adc76fc5faa7a82f4e4fdbec8850dc6f7bd
geeksploit <geeksploit@shootan.com>
2017-11-17 17:54:44 +0300

    Align the TextView to the start of the parent

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -46,7 +46,7 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     // COMPLETED (5) Add a TextView as the only child of the FrameLayout
     // COMPLETED (6) Give the TextView an ID "@+id/tv_item_number"
     // COMPLETED (7) Set the height and width to wrap_content
-    // TODO (8) Align the TextView to the start of the parent
+    // COMPLETED (8) Align the TextView to the start of the parent
     // TODO (9) Center the TextView vertically in the layout
     // TODO (10) Set the font family to monospace
     // TODO (11) Set the text size to 42sp
