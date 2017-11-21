0359c0d546289cbdea45a8ade425e46c3b0769c8
geeksploit <geeksploit@shootan.com>
2017-11-21 16:08:31 +0300

    Cancel the current toast

--- Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -106,9 +106,10 @@ public class MainActivity extends AppCompatActivity implements GreenAdapter.List
     }
 
     // COMPLETED (10) Override ListItemClickListener's onListItemClick method
-    // TODO (11) In the beginning of the method, cancel the Toast if it isn't null
+    // COMPLETED (11) In the beginning of the method, cancel the Toast if it isn't null
     // TODO (12) Show a Toast when an item is clicked, displaying that item number that was clicked
     @Override
     public void onListItemClick(int itemNumber) {
+        if (mToast != null) mToast.cancel();
     }
 }
