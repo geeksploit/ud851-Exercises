a687fda70a8ade2761817c6326aa4dd0850bf314
geeksploit <geeksploit@shootan.com>
2017-11-24 10:03:14 +0300

    Set the text to something that indicates this is the default text

--- Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 // Do steps 2 - 5 in activity_child.xml
 // COMPLETED (2) Change the ConstraintLayout to a FrameLayout and make appropriate adjustments
 // COMPLETED (3) Give your TextView an ID of tv_display
-// TODO (4) Set the text to something that indicates this is default text being displayed
+// COMPLETED (4) Set the text to something that indicates this is default text being displayed
 // TODO (5) Make the text size a little larger
 
 // Do steps 6 & 7 in ChildActivity.java
