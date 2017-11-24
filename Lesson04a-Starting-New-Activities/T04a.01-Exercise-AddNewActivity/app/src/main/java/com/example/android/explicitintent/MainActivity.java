173b843fcd2d13b54156fa60fceabb8a5545a930
geeksploit <geeksploit@shootan.com>
2017-11-24 10:08:15 +0300

    Make the text a little bit larger

--- Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 // COMPLETED (2) Change the ConstraintLayout to a FrameLayout and make appropriate adjustments
 // COMPLETED (3) Give your TextView an ID of tv_display
 // COMPLETED (4) Set the text to something that indicates this is default text being displayed
-// TODO (5) Make the text size a little larger
+// COMPLETED (5) Make the text size a little larger
 
 // Do steps 6 & 7 in ChildActivity.java
 // TODO (6) Create a TextView field to display your message
