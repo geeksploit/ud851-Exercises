195527aed85cb65c031d2ae7491a13aaad3a798c
geeksploit <geeksploit@shootan.com>
2017-11-25 22:39:52 +0300

    Start a new Activity instead of showing the Toast

--- Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -68,10 +68,8 @@ public class MainActivity extends AppCompatActivity {
                 // COMPLETED (2) Create an Intent to start ChildActivity
                 Intent intent = new Intent(context, destinationActivity);
 
-                // TODO (3) Replace the Toast with code to start ChildActivity
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-
+                // COMPLETED (3) Replace the Toast with code to start ChildActivity
+                startActivity(intent);
             }
         });
     }
