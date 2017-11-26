a28c01f52ef82a2078610152e50a96e1122e6ff9
geeksploit <geeksploit@shootan.com>
2017-11-26 12:47:37 +0300

    Check if this Intent has the extra we passed from MainActivity

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -36,10 +36,11 @@ public class ChildActivity extends AppCompatActivity {
         // COMPLETED (3) Use the getIntent method to store the Intent that started this Activity in a variable
         Intent intentThatStartedThisActivity = getIntent();
 
-        // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
-
+        // COMPLETED (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
             // TODO (5) If the Intent contains the correct extra, retrieve the text
 
             // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
+        }
     }
 }
