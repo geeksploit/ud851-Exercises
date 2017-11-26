0cbb8405f139799d3efd143cb07d87be2743534d
geeksploit <geeksploit@shootan.com>
2017-11-26 12:44:19 +0300

    Put the String from the EditText in the Intent

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -73,7 +73,8 @@ public class MainActivity extends AppCompatActivity {
                  */
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
-                // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+                // COMPLETED (2) Use the putExtra method to put the String from the EditText in the Intent
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, stringToPassToAnotherActivity);
 
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
