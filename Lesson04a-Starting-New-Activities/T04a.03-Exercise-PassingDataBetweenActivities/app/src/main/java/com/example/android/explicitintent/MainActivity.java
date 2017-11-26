60f724fb2d9249835733de28586aaedb7e19fb4b
geeksploit <geeksploit@shootan.com>
2017-11-26 12:40:32 +0300

    Retrieve the text to be passed to another Activity and store it in a variable

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                // TODO (1) Retrieve the text from the EditText and store it in a variable
-
+                // COMPLETED (1) Retrieve the text from the EditText and store it in a variable
+                String stringToPassToAnotherActivity = mNameEntry.getText().toString();
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
                  * just used "this" or "MainActivity.this" in the method call below. However, we
