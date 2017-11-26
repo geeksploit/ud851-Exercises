270977f086aa117b0e6e843d0b48ddf51df6bd91
geeksploit <geeksploit@shootan.com>
2017-11-26 12:50:00 +0300

    Retrieve the String from the Intent

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -38,7 +38,8 @@ public class ChildActivity extends AppCompatActivity {
 
         // COMPLETED (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
         if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
-            // TODO (5) If the Intent contains the correct extra, retrieve the text
+            // COMPLETED (5) If the Intent contains the correct extra, retrieve the text
+            String incomingText = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
 
             // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
         }
