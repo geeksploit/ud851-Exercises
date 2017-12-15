65035ac55f7fd5eaa435cf640868e79820485d8c
geeksploit <geeksploit@shootan.com>
2017-12-15 04:17:22 +0300

    Create a class called SettingsFragment

--- Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -16,10 +16,10 @@ package android.example.com.visualizerpreferences;
  * limitations under the License.
  */
 
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -46,7 +46,7 @@ public class SettingsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (2) Create a class called SettingsFragment that extends PreferenceFragmentCompat
+    // COMPLETED (2) Create a class called SettingsFragment that extends PreferenceFragmentCompat
     // TODO (3) In res->xml create a file called pref_visualizer
     // TODO (4) In pref_visualizer create a preference screen containing a single check box preference
     // This check box preference should have a default value of true, the key 'show_bass', a
