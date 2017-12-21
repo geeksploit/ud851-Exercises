93707ae7b138be66c034b9e98470c2e388f84258
geeksploit <geeksploit@shootan.com>
2017-12-21 09:58:35 +0300

    Create a constant for the table name

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -10,6 +10,7 @@ public class WaitlistContract {
     public static final class WaitListEntry implements BaseColumns {
         // TODO (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
+        public static final String TABLE_NAME = "waitlist";
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
