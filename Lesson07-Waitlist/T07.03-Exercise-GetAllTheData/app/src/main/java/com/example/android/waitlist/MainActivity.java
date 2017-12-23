443df6912ea4fdef952849cde1b1bb6fbd6f65f8
geeksploit <geeksploit@shootan.com>
2017-12-23 22:31:11 +0300

    Store the cursor pointing before the first guest

--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -45,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         // COMPLETED (4) call insertFakeData from TestUtil and pass the database reference mDb
         TestUtil.insertFakeData(mDb);
 
-        // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
+        // COMPLETED (7) Run the getAllGuests function and store the result in a Cursor variable
+        Cursor allGuestsCursor = getAllGuests();
 
         // TODO (12) Pass the resulting cursor count to the adapter
 
