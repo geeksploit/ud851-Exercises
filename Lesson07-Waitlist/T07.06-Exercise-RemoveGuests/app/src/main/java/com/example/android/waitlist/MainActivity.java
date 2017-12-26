da2db553c8e3224d2881e524e25fb3223b18ca55
geeksploit <geeksploit@shootan.com>
2017-12-26 19:29:59 +0300

    Get the viewHolder's itemView's tag and store it in a long variable id

--- Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         // COMPLETED (3) Create a new ItemTouchHelper with a SimpleCallback that handles both LEFT and RIGHT swipe directions
         // COMPLETED (4) Override onMove and simply return false inside
         // COMPLETED (5) Override onSwiped
-        // TODO (8) Inside, get the viewHolder's itemView's tag and store in a long variable id
+        // COMPLETED (8) Inside, get the viewHolder's itemView's tag and store in a long variable id
         // TODO (9) call removeGuest and pass through that id
         // TODO (10) call swapCursor on mAdapter passing in getAllGuests() as the argument
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                long id = (long) viewHolder.itemView.getTag();
             }
         });
 
