5dbc8f271d420096bd9bf6ef31dc2f1efb869c3c
geeksploit <geeksploit@shootan.com>
2017-12-30 17:34:23 +0300

    Access the DroidTermsExample to get Cursor

--- Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -16,12 +16,15 @@
 
 package com.udacity.example.quizexample;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 /**
  * Gets the data from the ContentProvider and shows a series of flash cards.
@@ -93,9 +96,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // COMPLETED (1) Create AsyncTask with the following generic types <Void, Void, Cursor>
-    // TODO (2) In the doInBackground method, write the code to access the DroidTermsExample
+    // COMPLETED (2) In the doInBackground method, write the code to access the DroidTermsExample
     // provider and return the Cursor object
     // TODO (4) In the onPostExecute method, store the Cursor object in mData
     class AsyncTaskToRetrieveCursor extends AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            ContentResolver contentResolver = getContentResolver();
+            return contentResolver.query(DroidTermsExampleContract.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null);
+        }
     }
 }
