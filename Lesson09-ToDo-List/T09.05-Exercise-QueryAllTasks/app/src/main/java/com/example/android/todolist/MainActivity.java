1f2d80248d93e56692da66f18b3631274ec8bd28
geeksploit <geeksploit@shootan.com>
2018-01-04 12:57:07 +0300

    Query and load all task data in the background; sort by priority

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -29,6 +29,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
+import com.example.android.todolist.data.TaskContract;
+
 
 public class MainActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
@@ -144,12 +146,12 @@ public class MainActivity extends AppCompatActivity implements
             // loadInBackground() performs asynchronous loading of data
             @Override
             public Cursor loadInBackground() {
-                // Will implement to load data
-
-                // TODO (5) Query and load all task data in the background; sort by priority
-                // [Hint] use a try/catch block to catch any errors in loading data
-
-                return null;
+                // COMPLETED (5) Query and load all task data in the background; sort by priority
+                return getContentResolver().query(TaskContract.TaskEntry.CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        TaskContract.TaskEntry.COLUMN_PRIORITY);
             }
 
             // deliverResult sends the result of the load, a Cursor, to the registered listener
